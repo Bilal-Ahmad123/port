b4518a722a46ce04dc7010febba65c3fd6eb6c59
Bilal Ahmad <ba269953@gmail.com>
2023-12-26 01:52:42 +0500

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,71 @@
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_s93y60z",
+        "template_hba4pwc",
+        form.current,
+        "XqTlgAZbwRmbEY5j1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+            Contact
+          </p>
+        </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button
+          className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center"
+          type="submit"
+        >
+          Let's Collaborate
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import WorkIm from "../assets/projects/workImg.jpeg";
+import RealEst from "../assets/projects/realestate.jpg";
+import Uber from "../assets/projects/Uber.jpeg";
+import YelpCamp from "../assets/projects/YelpCamp.PNG";
+import Hotel from "../assets/projects/Hotel.jpeg";
+import BoxHill from "../assets/projects/Boxhill.jpeg";
+
+export const data = [
+  {
+    id: 2,
+    name: "BoxHill",
+    image: BoxHill,
+    github: "https://github.com/Bilal-Ahmad123/BoxHill",
+    live: "",
+  },
+  {
+    id: 3,
+    name: "Hotel Reservation System",
+    image: Hotel,
+    github: "https://github.com/Bilal-Ahmad123/Hotel-Reservation-System",
+    live: "",
+  },
+  {
+    id: 4,
+    name: "YelpCamp",
+    image: YelpCamp,
+    github: "https://github.com/Bilal-Ahmad123/YelpCamp",
+    live: "",
+  },
+  {
+    id: 5,
+    name: "Uber Mobile App",
+    image: Uber,
+    github: "https://github.com/Bilal-Ahmad123/Taxi",
+    live: "",
+  },
+];
